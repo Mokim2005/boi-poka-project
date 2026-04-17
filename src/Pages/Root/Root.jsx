d6fc052e63d704fac783cd3bdaf1e362a37663ed
mokim <mamokim2005@gmail.com>
2026-04-17 23:48:15 +0600

    updated responsiveness

--- src/Pages/Root/Root.jsx
+++ src/Pages/Root/Root.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
-import Navber from '../../Components/Header/Navber';
-import { Outlet } from 'react-router';
-import Footer from '../../Components/Footer/Footer';
+import React from "react";
+import Navber from "../../Components/Header/Navber";
+import { Outlet } from "react-router";
+import Footer from "../../Components/Footer/Footer";
 
 const Root = () => {
-    return (
-        <div className="min-h-screen flex flex-col">
-            <Navber></Navber>
-            <main className="flex-1">
-                <Outlet></Outlet>
-            </main>
-            <Footer></Footer>
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      
+      {/* Navbar */}
+      <header className="bg-white shadow-sm">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <Navber />
         </div>
-    );
+      </header>
+
+      {/* Main Content */}
+      <main className="flex-1 flex items-center justify-center">
+        <div className="w-full max-w-7xl px-4 sm:px-6 lg:px-8 py-6">
+          <Outlet />
+        </div>
+      </main>
+
+      {/* Footer */}
+      <footer className="bg-gray-900 text-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 text-center">
+          <Footer />
+        </div>
+      </footer>
+
+    </div>
+  );
 };
 
 export default Root;
